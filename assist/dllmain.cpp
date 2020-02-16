fafa170af742ecce97d955d11850adb2a76f509f
Nikolai <megoze@ro.ru>
2020-02-17 02:18:12 +0500

    Загрузка переработанного проекта
    Проект переработан, более не используется создание окна Windows,в качестве средства отображения информации

---
+++ assist/dllmain.cpp
@@ -0,0 +1,79 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include <iostream>
+#include "Header.h" // TODO : peremestit' strukturu.
+#include <string>
+
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+#pragma pack(push,1)
+typedef struct
+{
+    BYTE Padding1[3];
+    BYTE OEM_Name[8];
+} NTFS_BootRecord;
+#pragma pack(pop)
+
+std::string byte2ch(BYTE* data, int size)
+{
+    return std::string((char*)data, size);
+}
+
+extern "C" __declspec(dllexport) void CheckPhysDrives(bool* rez,int DriveNum,std::string fileName)
+{
+
+    std::string fullPath = fileName + std::to_string(DriveNum);
+    HANDLE fileHandle = CreateFileA(
+        fullPath.c_str(),
+        GENERIC_READ,
+        FILE_SHARE_READ | FILE_SHARE_WRITE,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL);
+
+    if (fileHandle != INVALID_HANDLE_VALUE)
+    {
+        *rez = true;
+    }
+    else
+    {
+        *rez = false;      
+    }
+}
+extern "C" __declspec(dllexport) void CheckVolume(bool* rez,std::string fileName,std::string letter)
+{
+    std::string fullPath = fileName + letter+":";
+    HANDLE fileHandle = CreateFileA(
+        fullPath.c_str(),
+        GENERIC_READ,
+        FILE_SHARE_READ | FILE_SHARE_WRITE,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL);
+    if (fileHandle != INVALID_HANDLE_VALUE)
+    {
+        *rez = true;
+    }
+    else
+    {
+        *rez = false;
+    }
+}
+
+
---
+++ spitsa_ntfs/spitsa_ntfs.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+typedef void (*checkPhys)(bool&,int,std::string); // TODO: replace to header
+typedef void (*checkVol)(bool&,std::string, std::string);
+int main()
+{   
+    HMODULE hLib = LoadLibrary(L"assist.dll");
+    if (hLib == NULL)
+    {
+        std::cout << "No able to attach dll !" << std::endl;
+    }
+    else
+    {
+        checkPhys dllCheckPhysDrives = (checkPhys)GetProcAddress(hLib, "CheckPhysDrives");
+        if (!dllCheckPhysDrives)
+        {
+            std::cout << "Error while getting func address (Phys)" << std::endl; //TODO : ERROR COLOR ??
+        }
+        else
+        {
+            int foundDriveCount = 0;
+            bool checkResult = false;
+            std::string fileName = "\\\\.\\PhysicalDrive";
+            std::cout << "       ------------------------------------" << std::endl;
+            for (int DriveNum = 0; DriveNum < 30; DriveNum++)
+            {
+                checkResult = false;
+                dllCheckPhysDrives(checkResult, DriveNum,fileName);
+                if (checkResult == true)
+                {  
+                    std::cout << "       "<<fileName << DriveNum << "             exist " << std::endl;
+                    foundDriveCount++;
+                }
+            }
+            std::cout << "\n"<<"       Summary physical drives founded : " << foundDriveCount << std::endl;
+            std::cout << "       ------------------------------------\n"<<"\n" << std::endl;
+            if (foundDriveCount == 0)
+            {
+                std::cout << "\n" << "  No drives exist, stopping programm..." << std::endl;
+                std::cin;  //TODO : ERROR COLOR ??
+            }
+            else
+            {
+                checkVol dllCheckVolume = (checkVol)GetProcAddress(hLib, "CheckVolume");
+                if (!dllCheckVolume)
+                {
+                    std::cout << "Error while getting func address (Volumes)"<< std::endl; //TODO : ERROR COLOR ??
+                }
+                else
+                {
+                    fileName = fileName = "\\\\.\\";
+                    const int alphCount = 26;
+                    int foundVolumes = 0;
+                    std::string letters[alphCount] = { "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z" };
+                    for (int letterNum = 0; letterNum < 26; letterNum++)
+                    {
+                        checkResult = false;
+                        dllCheckVolume(checkResult, fileName, letters[letterNum]);
+                        if (checkResult == true)
+                        {
+                            std::cout << "       " << fileName<< letters[letterNum] << "                          exist \n";
+                            foundVolumes++;
+                        }
+                    };
+                    std::cout << "\n" << "       Summary volume drives founded : " << foundVolumes << std::endl;
+                    std::cout << "       ------------------------------------\n" << "\n" << std::endl;
+                    if (foundVolumes == 0)
+                    {
+                        std::cout << "\n" << "  No drives exist, stopping programm...";
+                        std::cin;  //TODO : ERROR COLOR ??
+                    }
+                    else
+                    {   //TODO: GET INFO?
+                    }
+                }
+            }
+        }
+    }
+}
+
