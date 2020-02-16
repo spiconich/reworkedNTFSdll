6fa521667429bae3194d2e680f826b15084e0937
Nikolai <megoze@ro.ru>
2020-02-17 03:33:02 +0500

    Создание заголовочных

---
+++ assist/dllHeader.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <Windows.h>
+#pragma pack(push,1)
+typedef struct
+{
+	BYTE Padding1[3];
+	BYTE OEM_Name[8];
+} NTFS_BootRecord;
+
+#pragma pack(pop)
---
+++ spitsa_ntfs/diffDef.h
@@ -0,0 +1,4 @@
+#pragma once
+typedef void (*checkPhys)(bool&, int, std::string);
+typedef void (*checkVol)(bool&, bool&, bool&, std::string, std::string,std::string&);
+
