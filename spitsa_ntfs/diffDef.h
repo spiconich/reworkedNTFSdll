ff25a853ce046467d78fabfcbc0ea9be79f2e8c1
Nikolai <megoze@ro.ru>
2020-02-17 14:40:27 +0500

    Добавлена функция проверки выбранного тома на наличие типа NTFS

--- spitsa_ntfs/diffDef.h
+++ spitsa_ntfs/diffDef.h
@@ -1,4 +1,5 @@
 #pragma once
+typedef void (*checkIsNTFS)(bool&,bool&,bool&,bool&, std::string);
 typedef void (*checkPhys)(bool&, int, std::string);
 typedef void (*checkVol)(bool&, bool&, bool&, std::string, std::string,std::string&);
 
